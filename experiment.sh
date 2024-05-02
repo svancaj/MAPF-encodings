554e96b0b19c7521a54c3106cf0338c91698cda1
svancaj <jirka.svanc@gmail.com>
2024-05-02 12:16:04 +0200

    improve experiment script

--- experiment.sh
+++ experiment.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-timeout=100
+timeout=30
 
 for instance in instances/scenarios/random16-1.scen
 do
-	for encoding in shift_pebble_mks_all shift_pebble_soc_all 
+	for vars in at pass shift
 	do
-		./release/MAPF -s $instance -t $timeout -e $encoding -a 1 -i 1 -l 1 -f results.res
+		for movement in parallel pebble
+		do
+			for cost in mks soc 
+			do
+				for lazy in all
+				do
+					encoding="${vars}_${movement}_${cost}_${lazy}"
+					./release/MAPF -s $instance -t $timeout -e $encoding -a 1 -i 1 -l 1 -f results.res
+				done
+			done
+		done
 	done
 done
